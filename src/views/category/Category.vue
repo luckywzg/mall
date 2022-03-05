eebaf63b03edfc110a25d3b80a29d1ed8b3c845a
luckywzg <2024506078@qq.com>
2022-03-05 21:52:10 +0800

    修改文件

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="wrapper" ref="wrapper">
-  <!--  使用better-scroll-->
+    <!--  使用better-scroll-->
     <h4>分类</h4>
   </div>
 </template>
 
 <script>
-  import BScroll from "better-scroll"
-  export default {
-    name: "Category",
-    data(){
-      return {
-        scroll: null
-      }
-    },
-    created() {
+import BScroll from "better-scroll";
+export default {
+  name: "Category",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  created() {
     //  不能使用created，此时的el还没有挂载
-    },
-    mounted() {
-      console.log(document.querySelector('.wrapper'));
-      this.scroll = new BScroll(document.querySelector('.wrapper'),{
-
-      })
-    }
-  }
+  },
+  mounted() {
+    console.log(document.querySelector(".wrapper"));
+    this.scroll = new BScroll(document.querySelector(".wrapper"), {});
+  },
+};
 </script>
 
 <style scoped>
-  .wrapper {
-    height: 150px;
-    background-color: royalblue;
-    overflow: hidden;
-  }
+.wrapper {
+  height: 150px;
+  overflow: hidden;
+}
 </style>
